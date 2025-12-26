4a9df306988fcea95b0abf07f283b325d0e8a5de
Excellent-Error <omidijigoodness8@gmail.com>
2025-12-26 20:53:52 +0100

    Add files via upload

---
+++ ⚡ Excellent Error ⚡Bankai Check.py
@@ -0,0 +1,9 @@
+print("Welcome to Goodness also  known as {Excellent Error}the bankai classification program")
+
+bankai=int(input("Enter  number of bankai : "))
+if bankai<=5:
+    print("You are at beginner level. ")
+else:
+    print("You are an otaku. ")
+    
+print("Thank you for partaking in this programme")
